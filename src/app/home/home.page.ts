67456396123c58ce52e0d50fec3d93c3a772286d
JimyCalvo <jbcm1152@gmail.com>
2023-06-06 16:12:56 -0500

    ionc-prueba-01

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { CallbackID, Capacitor } from '@capacitor/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,83 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  isActionSheetOpen = false;
+  coordinate: any;
+  watchCoordinate: any;
+  watchId: any;
 
-  constructor() {}
+  constructor(private zone: NgZone) { }
+
+  async requestPermissions() {
+    const permResult = await Geolocation.requestPermissions();
+    console.log('Perm request result: ', permResult);
+  }
+
+  getCurrentCoordinate() {
+    if (!Capacitor.isPluginAvailable('Geolocation')) {
+      console.log('Plugin geolocation not available');
+      return;
+    }
+    Geolocation.getCurrentPosition().then(data => {
+      this.coordinate = {
+        latitude: data.coords.latitude,
+        longitude: data.coords.longitude,
+        accuracy: data.coords.accuracy
+      };
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+
+  watchPosition() {
+    try {
+      this.watchId = Geolocation.watchPosition({}, (position, err) => {
+        if (position != null) {
+          console.log('Watch', position);
+          this.zone.run(() => {
+            this.watchCoordinate = {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            };
+          });
+        }else{
+          console.error('The position is null');
+        }
+
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  clearWatch() {
+    if (this.watchId != null) {
+      Geolocation.clearWatch({ id: this.watchId });
+    }
+  }
+
+
+
+
+  public actionSheetButtons = [
+    {
+      text: 'Enable Permissions',
+      handler: () => {
+        this.requestPermissions();
+      },
+    },
+
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  setOpen(isOpen: boolean) {
+    this.isActionSheetOpen = isOpen;
+  }
 
 }
